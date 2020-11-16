af6706bae2e27fa98be16799ead0df0ee109f6a5
Mike Wills <mwills@bluemodus.com>
2020-11-15 16:31:55 -0800

    Initial build for Kentico Xperience 11.0

---
+++ UpsertAlternativeForms/Helpers/Guard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KenticoCommunity.UpsertAlternativeForms.Helpers
+{
+    /// <summary>
+    /// Provide helper methods for checking parameter arguments
+    /// </summary>
+    public static class Guard
+    {
+        /// <summary>
+        /// Throws an exception if an argument is null
+        /// </summary>
+        /// <param name="value">The value to be tested</param>
+        /// <param name="name">The name of the argument</param>
+        public static void ArgumentNotNull(object value, string name = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = nameof(value);
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(name, $"Argument { name } must not be null");
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if a string argument is null or empty
+        /// </summary>
+        /// <param name="value">The value to be tested</param>
+        /// <param name="name">The name of the argument</param>
+        public static void ArgumentNotNullOrEmpty(string value, string name = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = nameof(value);
+            }
+
+            ArgumentNotNull(value, name);
+
+            if (value == string.Empty)
+            {
+                throw new ArgumentException($"Argument { name } must not be an empty string", name);
+            }
+        }
+    }
+}
---
+++ UpsertAlternativeForms/UpsertAlternativeFormInfoProvider.cs
@@ -0,0 +1,43 @@
+﻿using CMS;
+using CMS.FormEngine;
+using KenticoCommunity.UpsertAlternativeForms;
+using KenticoCommunity.UpsertAlternativeForms.Helpers;
+
+[assembly: RegisterCustomProvider(typeof(UpsertAlternativeFormInfoProvider))]
+
+namespace KenticoCommunity.UpsertAlternativeForms
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Kentico custom provider for alternative forms to enable using alternative forms named "upsert"
+    /// for all three form modes supported for pages: "insert", "update", and "newculture".
+    /// </summary>
+    public class UpsertAlternativeFormInfoProvider : AlternativeFormInfoProvider
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Overrides default Kentico behavior by allowing an alternative form named "upsert" to be used if
+        /// forms named "insert", "update", or "newculture" do not exist.
+        /// </summary>
+        /// <param name="alternativeFormFullName">The provided name of the alternative form</param>
+        /// <param name="useHashtable">Optional flag to use a hash table</param>
+        /// <returns>An AlternativeFormInfo object</returns>
+        protected override AlternativeFormInfo GetInfoByFullName(string alternativeFormFullName, bool useHashtable = true)
+        {
+            Guard.ArgumentNotNullOrEmpty(alternativeFormFullName, nameof(alternativeFormFullName));
+
+            var providedForm = base.GetInfoByFullName(alternativeFormFullName, useHashtable);
+            if (providedForm != null)
+            {
+                return providedForm;
+            }
+
+            if(!AlternativeFormNameHelper.IsBuiltInPageTypeFormName(alternativeFormFullName))
+            {
+                return null;
+            }
+            var upsertFormName = AlternativeFormNameHelper.CreateUpsertFullName(alternativeFormFullName);
+            return base.GetInfoByFullName(upsertFormName);
+        }
+    }
+}
